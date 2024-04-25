aef9cc9d49dde3842a1deda51a4ea2e6a082b700
Ella Hoffman <e-hoffman.2@onu.edu>
2024-04-25 08:52:36 -0400

    Part 2

--- Source.cpp
+++ Source.cpp
@@ -11,21 +11,58 @@ void pressEnterToContinue() {
 	while (cin.get() != '\n') continue;
 }
 
-bool readFromFile(string filename) {
-	ifstream inFile;
-	string movieTitle;
-	int year;
-	inFile.open(filename);
+//bool readFromFile(string filename) {
+	//ifstream inFile;
+	//string movieTitle;
+	//int year;
+	//inFile.open(filename);
 
+	//if (inFile.fail()) {
+		//cout << "read error - sorry\n";
+		//return false;
+	//}
+	//while (true) {
+		//inFile >> movieTitle >> year;
+		//if (inFile.fail()) break;
+
+		//cout << movieTitle << ": " << year << endl;
+	//}
+	//inFile.close();
+	//return true;
+//}
+
+bool readFromCSVFile(string filename) {
+	ifstream inFile;
+	istringstream thisLine;
+	string movieInfo;
+	string movieTitle;
+	string year;
+	string director;
+
+	inFile.open(filename);
 	if (inFile.fail()) {
 		cout << "read error - sorry\n";
 		return false;
 	}
-	while (true) {
-		inFile >> movieTitle >> year;
-		if (inFile.fail()) break;
 
-		cout << movieTitle << ": " << year << endl;
+	while (true) {
+		getline(inFile, movieInfo);
+		if (inFile.fail()) break;
+		thisLine.clear();
+		thisLine.str(movieInfo);
+		getline(thisLine, movieTitle, ',');
+		getline(thisLine, year, ',');
+
+		vector<string> directorList;
+		string dir;
+		while (getline(thisLine, dir, ','))
+			directorList.push_back(dir);
+
+		director = directorList[0];
+		for (int i = 1; i < directorList.size(); i++) {
+			director = director + " and " + directorList[i];
+		}
+		cout << movieTitle << " is a " << year << " film directed by " << director << endl;
 	}
 	inFile.close();
 	return true;
@@ -36,7 +73,7 @@ int main() {
 	cout << "Lab 12 Intro to Text Files!\n";
 	cout << "Enter filename: ";
 	getline(cin, datafile);
-	bool status = readFromFile(datafile);
+	bool status = readFromCSVFile(datafile);
 	if (status) cout << "Success!\n";
 	else cout << "failure. :-(\n";
 	pressEnterToContinue();
